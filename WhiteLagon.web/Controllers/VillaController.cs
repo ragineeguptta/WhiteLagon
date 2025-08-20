35c66453f1a3d66e8068e4af1b02e449dc3c1910
Raginee <raginee.gupta@hirdhav.com>
2025-08-20 12:33:54 +0530

    ⭐️ Section 4: Villa CRUD ⭐️ ⌨️ (3:07:28) Edit and Delete buttons in UI ⌨️ (3:11:03) Get Action Method for Update Villa ⌨️ (3:13:58) Update Villa UI ⌨️ (3:15:54) Multiple ways to retrieve in EF Core ⌨️ (3:18:07) Not Found Page ⌨️ (3:20:43) Update in Action ⌨️ (3:25:21) Delete Villa UI ⌨️ (3:27:10) Delete in Action

--- WhiteLagon.web/Controllers/VillaController.cs
+++ WhiteLagon.web/Controllers/VillaController.cs
@@ -39,5 +39,52 @@ namespace WhiteLagon.web.Controllers
             }
             return View();
         }
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u=>u.Id == villaId);
+            if (obj == null)
+            {
+                return RedirectToAction("Error","Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb is not null)
+            {
+
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
