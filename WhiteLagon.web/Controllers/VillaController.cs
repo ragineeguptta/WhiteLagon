63f761a1d6b73007616d652e1dac95070d80b32f
rgupta <raginee.gupta@etpgroup.com>
2025-10-06 15:47:24 +0530

    ⭐️ Section 8: Images and Villa ⭐️ ⌨️ (5:16:48) Add Form File in Villa Model ⌨️ (5:19:06) Handle Image Upload Part 1 ⌨️ (5:22:07) Upload Image in Action ⌨️ (5:26:58) Display Image in Upload UI ⌨️ (5:29:01) Handle Image in Update Villa ⌨️ (5:31:44) Solving Bug ⌨️ (5:33:01) Remove Image on Villa Delete

--- WhiteLagon.web/Controllers/VillaController.cs
+++ WhiteLagon.web/Controllers/VillaController.cs
@@ -8,9 +8,11 @@ namespace WhiteLagon.web.Controllers
     public class VillaController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public VillaController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public VillaController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -33,7 +35,21 @@ namespace WhiteLagon.web.Controllers
             }
             if (ModelState.IsValid)
             {
+                if(obj.Image != null)
+                {
+                    string fileName = Guid.NewGuid().ToString()+ Path.GetExtension(obj.Image.FileName);
+                    string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, @"images\VillaImages");
 
+                    using (var fileStream = new FileStream(Path.Combine(imagePath, fileName), FileMode.Create))
+                    {
+                        obj.Image.CopyTo(fileStream);
+                    }
+                    obj.ImageUrl = @"\images\VillaImages\" + fileName;
+                }
+                else
+                {
+                    obj.ImageUrl = "https://placehold.co/600x400";
+                }
                 _unitOfWork.Villa.Add(obj);
                 _unitOfWork.Save();
                 TempData["success"] = "Villa created successfully";
@@ -58,7 +74,26 @@ namespace WhiteLagon.web.Controllers
         {
             if (ModelState.IsValid && obj.Id > 0)
             {
+                if (obj.Image != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(obj.Image.FileName);
+                    string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, @"images\VillaImages");
 
+                    if (!string.IsNullOrEmpty(obj.ImageUrl))
+                    {
+                        var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, obj.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    using (var fileStream = new FileStream(Path.Combine(imagePath, fileName), FileMode.Create))
+                    {
+                        obj.Image.CopyTo(fileStream);
+                    }
+                    obj.ImageUrl = @"\images\VillaImages\" + fileName;
+                }
                 _unitOfWork.Villa.Update(obj);
                 _unitOfWork.Save();
                 TempData["success"] = "Villa updated successfully";
@@ -84,6 +119,14 @@ namespace WhiteLagon.web.Controllers
             Villa? objFromDb = _unitOfWork.Villa.Get(u => u.Id == obj.Id);
             if (objFromDb is not null)
             {
+                if (!string.IsNullOrEmpty(objFromDb.ImageUrl))
+                {
+                    var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, objFromDb.ImageUrl.TrimStart('\\'));
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
                 _unitOfWork.Villa.Remove(objFromDb);
                 _unitOfWork.Save();
                 TempData["success"] = "Villa deleted successfully";
--- Whitezlagon.Domain/Entities/Villa.cs
+++ Whitezlagon.Domain/Entities/Villa.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +21,8 @@ namespace WhiteLagon.Domain.Entities
         public int Sqft { get; set; }
         [Range(1,10)]
         public int Occupency { get; set; }
-
+        [NotMapped]
+        public IFormFile? Image { get; set; }
         public string? ImageUrl { get; set; }
         public DateTime Created_At { get; set; } = DateTime.UtcNow;
         public DateTime Updated_At { get; set; } = DateTime.UtcNow;
